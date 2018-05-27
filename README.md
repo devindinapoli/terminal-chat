ce36ca56bbee1db200f5aa5880a3ce8f786aa09d
Devin DiNapoli <devindinapoli@gmail.com>
2018-05-27 15:45:20 -0400

    Added animated loading spinners

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Terminal Chat
 
 A NodeJS application using the Pusher Chatkit.
+
+To run the application:
+
+```npm install``` in the root directory.
+
+Run ```node server.js``` to start the server.
+
+Next, in a seperate terminal window/tab, run ```node client.js```.
+
+Follow the prompt, and start chatting!
+
--- client.js
+++ client.js
@@ -3,6 +3,8 @@ const { JSDOM } = require('jsdom');
 const util = require('util');
 const prompt = require('prompt');
 const axios = require('axios');
+const readline = require('readline');
+const ora = require('ora');
 
 const nodeCompatibleChatkit = () => {
     const { window } = new JSDOM();
@@ -21,6 +23,8 @@ const createUser = async username => {
 };
 
 const main = async () => {
+
+    const spinner = ora();
     try {
         // Start prompt with an empty message
         prompt.start();
@@ -37,24 +41,76 @@ const main = async () => {
         ];
 
         const { username } = await get(usernameSchema);
+
+        spinner.start('Authenticating...');
         await createUser(username);
+        spinner.succeed(`Logged In As ${ username }`);
 
         const chatManager = new ChatManager({
             instanceLocator: 'v1:us1:7af82cb7-14d4-4157-8bb5-771964b5579b',
             userId: username,
             tokenProvider: new TokenProvider({ url: 'http://localhost:3001/authenticate' }),
         });
-        const currentUser = await chatManager.connect();
 
+        spinner.start('Connecting To Pusher...')
+        const currentUser = await chatManager.connect();
+        spinner.succeed('Connected!')
+
+        spinner.start('Finding Available Rooms...');
         const joinableRooms = await currentUser.getJoinableRooms();
+        spinner.succeed('Rooms Found!');
+
         const availableRooms = [...currentUser.rooms, ...joinableRooms];
 
         console.log('Available Rooms:');
         availableRooms.forEach((room, index) => {
             console.log(`${ index } - ${ room.name }`);
+        })
+
+        const roomSchema = [
+
+            {
+                description: 'Select A Room',
+                name: 'room',
+                conform: v => {
+                    if (v >= availableRooms.length) {
+                        return false
+                    }
+                    return true
+                },
+                message: 'Room Must Only Be Numbers',
+                required: true
+            }
+        ]
+
+        const { room: chosenRoom } = await get(roomSchema);
+        const room = availableRooms[chosenRoom];
+
+        spinner.start(`Joining ${ chosenRoom }...`);
+        await currentUser.subscribeToRoom({
+            roomId: room.id,
+            hooks: {
+                onNewMessage: message => {
+                    const { senderId, text } = message
+                    
+                    if(senderId === username) return
+                    console.log(`${ senderId }: ${ text }`)
+                }
+            },
+            messageLimit: 0
         });
 
+        spinner.succeed(`Joined ${ room.name }!`)
+
+        console.log(`Now In ${ room.name }`);
+
+        const input = readline.createInterface({ input: process.stdin });
+        input.on('line', async text => {
+            await currentUser.sendMessage({ roomId: room.id, text });
+        })
+
     } catch(err) {
+        spinner.fail();
         console.log(`${err}`);
         process.exit(1);
     }
